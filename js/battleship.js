974c5a1f8218b948ee2dae3b1f321af5b4b16d54
James <tytechortz@gmail.com>
2018-10-01 21:36:29 -0600

    adds array of ship positions

--- js/battleship.js
+++ js/battleship.js
@@ -1,3 +1,7 @@
+
+var player1Positions = [];
+
+
 for(let y = 0; y < 12; y++){
     if (y > 0) {
         $('.game').append(`<div class='game-column game-column-${y}'></div>`)
@@ -11,13 +15,26 @@ for(let y = 0; y < 12; y++){
         gameSquare.addClass(`square-${y-1}-${x}`)
         if (x < 10) {
             gameSquare.click(function() {
-            board[x][y-1].hit = true;
+            board[x][y-1].setShip = true;
+            if (setShip = true) {
+                player1Positions.push(gameSquare);
+                console.log(player1Positions);
+           }
+            $(`.square-${y-1}-${x}`).css('background-color', '#000000');
             console.log(board);
             });
         }
         $(`.game-column-${y}`).append(gameSquare)
+        
     }
+   
 }
+
+$('.game-column').droppable( {
+    drop:function(){
+        $(this).append
+    }
+});
 
 const letters = ['A','B','C','D','E','F','G','H','I','J'];
 const nums = ['1','2','3','4','5','6','7','8','9','10'];
