0e30db0e06809baad226c0b0fbf8eb518fd42a39
James <tytechortz@gmail.com>
2018-10-02 20:54:09 -0600

    adds set button to hide ship positions

--- js/battleship.js
+++ js/battleship.js
@@ -16,15 +16,18 @@ for(let y = 0; y < 12; y++){
         const gameSquare = $(`<div>${x},${y-1}</div>`)
         gameSquare.addClass('square')
         gameSquare.addClass(`square-${x}-${y-1}`)
-        gameSquare.attr('xCoordinate', x)
-        gameSquare.attr('yCoordinate', y-1)
+        gameSquare.attr('x', x)
+        gameSquare.attr('y', y-1)
         if (x < 10) {
             //creates new array of ship positions based on user clicking on squares
             gameSquare.click(function() {
             board[y-1][x].setShip = true;
             if (setShip = true) {
-                player1Positions.push(gameSquare);
+                
+                //console.log(gameSquare.attr('x'));
+                //console.log(gameSquare.attr('y'));
                 console.log(player1Positions);
+                
            }
             $(`.square-${x}-${y-1}`).css('background-color', '#000000');
             console.log(board);
@@ -35,6 +38,14 @@ for(let y = 0; y < 12; y++){
     }
    
 }
+$('#playerOneSet').click(function() {
+    $('.square').css('background-color', '');
+    // $('.square').css('background-color', 'dodgerblue');
+    // $('#number-column').css('background-color', 'silver');
+    // $('#letter-column').css('background-color', 'silver');
+    console.log(board);  
+});
+
 //adds id's to columns not in the playing area
 for(let i=0; i<11; i++) {
     $(`.square-10-${i}`).attr('id', 'letter-column');
@@ -61,28 +72,28 @@ for(let j=0; j<nums.length; j++) {
     $(`.square-${j}-10`).text(nums[j]);
 }
 // allows rotation of game pieces on double click
-var rotation = 0;
+// var rotation = 0;
 
-jQuery.fn.rotate = function(degrees) {
-    $(this).css({'-webkit-transform' : 'rotate('+ degrees +'deg)',
-                 '-moz-transform' : 'rotate('+ degrees +'deg)',
-                 '-ms-transform' : 'rotate('+ degrees +'deg)',
-                 'transform' : 'rotate('+ degrees +'deg)'});
-};
+// jQuery.fn.rotate = function(degrees) {
+//     $(this).css({'-webkit-transform' : 'rotate('+ degrees +'deg)',
+//                  '-moz-transform' : 'rotate('+ degrees +'deg)',
+//                  '-ms-transform' : 'rotate('+ degrees +'deg)',
+//                  'transform' : 'rotate('+ degrees +'deg)'});
+// };
 
-$('.rotate').dblclick(function() {
-    rotation += 90;
-    $(this).rotate(rotation);
-});
+// $('.rotate').dblclick(function() {
+//     rotation += 90;
+//     $(this).rotate(rotation);
+// });
 
 //firing buttons push values to player1FiredAt array
        
-    $('#player-one-letters button').click(function() {       
-        player1FiredAt.push($(this).val());
-            console.log(player1FiredAt);
-            console.log(player1FiredAt[0]);
-            console.log(player1FiredAt[1]);
-    });
+    // $('#player-one-letters button').click(function() {       
+    //     player1FiredAt.push($(this).val());
+    //         console.log(player1FiredAt);
+    //         console.log(player1FiredAt[0]);
+    //         console.log(player1FiredAt[1]);
+    // });
 
       
 
